31529e8aeec2b6512829e52ed3031346f3c202be
dulabdul <dul.abdoel21@gmail.com>
2021-08-30 22:18:02 +0700

    latihan 27JS

---
+++ FunctionJS/latihan27.js
@@ -0,0 +1,15 @@
+// Funtion Deklrasi
+function JumlahDuaBlg(a,b) {
+	var  total = a * b;
+
+	return total;
+}
+alert(JumlahDuaBlg(10,3));
+//Function Expreksi
+
+var JumlahTigaBlg = function (a,b,c) {
+	var total = a * b * c;
+
+	return total;
+}
+alert(JumlahTigaBlg(10,2,3));
