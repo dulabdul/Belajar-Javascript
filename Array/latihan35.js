9f78f7951cd7963b5e2d22cad680e6aa1260ff3a
dulabdul <dul.abdoel21@gmail.com>
2021-08-31 17:33:04 +0700

    latihan35JS

---
+++ Array/latihan35.js
@@ -0,0 +1,36 @@
+// Manipulasi Array
+
+// Menambahkan Isi Array
+var myArr = ['abdul', 'rahman', 'ganteng'];
+// Menampilkan Isi Array
+// for (var i = 0; i < myArr.length; i++) {
+// 	console.log('Halooo...' + myArr[i]);
+// }
+
+//Method Pada Array
+// 1.Join
+
+// console.log(myArr.join(''))
+
+// 2. Push
+
+// myArr.push('Asep','Joni');
+// console.log(myArr.join('-'))
+
+// 3. Pop
+
+// myArr.pop();
+// myArr.pop();
+// console.log(myArr.join('-'))
+
+// 4. Unshift
+
+// myArr.unshift('Kaseppp','Linda')
+
+// console.log(myArr.join('-'))
+
+// 5, Shift
+
+// myArr.shift()
+// myArr.shift()
+// console.log(myArr.join('-'))
